830f5bb80269eec17a103f3abfd44d868e107900
Anna Drozhzhina <mdtln@yandex.ru>
2023-04-24 11:31:39 +0300

    change of main.py

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 import requests
 
-TOKEN = 'y0A'
-
 
 class YandexDisk:
-    def __init__(self, token):
+    def __init__(self, token: str):
         self.token = token
 
-    def _get_upload_link(self, disk_file_path):
+    def _upload_file_to_disk(self, disk_file_path):
         link_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         headers = {
             'Content-Type': 'application/json',
-            'Authorization': f'OAuth{self.token}'
+            'Authorization': 'OAuth {}'.format(self.token)
         }
         params = {'path': disk_file_path, 'overwrite': 'true'}
-        response = requests.get(url=link_url, headers=headers, params=params)
-        return response.json()
-
-    def upload_file_to_disk(self, disk_file_path, file_name):
-        data = self._get_upload_link(disk_file_path=disk_file_path)
-        url = data.get('href')
-        response = requests.put(url=url, data=open(file_name, 'rb'))
+        response = requests.get(url=link_url, headers=headers, params=params).json()
+        url = response.get('href','')
+        response = requests.put(url=url, data=open(disk_file_path, 'rb'))
         if response.status_code == 201:
             print('Success!')
 
 
 if __name__ == '__main__':
+    TOKEN = 'y0'
     ya = YandexDisk(token=TOKEN)
-    ya.upload_file_to_disk('test_17_04.txt', 'test.txt')
+    result = ya._upload_file_to_disk('test.txt')
